a88a7cc3d77c38fd221c4403f0524371ceea558e
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-11 20:18:31 +0530

    Add files via upload

---
+++ Binary.java
@@ -0,0 +1,13 @@
+public class Binary {
+    public static void main(String args[]){
+        int a = 20;
+        int b = 2;
+
+        System.out.println("add : " +(a+b));
+        System.out.println("sub : " +(a-b));
+        System.out.println("multiply : " +(a*b));
+        System.out.println("division : " +(a/b));
+        System.out.println("modulo(remainder) : " +(a%b));
+    }
+    
+}
