5bd6bab88b1ce840e5d3809a6aa6b853ad6c9d55
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-11 20:16:09 +0530

    Add files via upload

---
+++ AreaofSquare.java
@@ -0,0 +1,17 @@
+/* In a program, input the side of a square. You have to output the area of the
+square.
+(Hint : area of a square is (side x side)) */
+
+import java.util.*;
+public class AreaofSquare {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+          float side = sc.nextFloat();
+
+          float area;
+          area = side*side;
+          System.out.println("area of square : " +area);
+    }
+    
+}
