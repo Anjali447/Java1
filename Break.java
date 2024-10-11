a5345550d78a69bf2ee6cde833f8cb2b291f2128
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-11 20:19:35 +0530

    Add files via upload

---
+++ Break.java
@@ -0,0 +1,14 @@
+public class Break {
+    public static void main(String args[]){
+        int i;
+       for(i = 1; i <= 5; i++){
+            if(i == 4){
+                break;
+            }
+            System.out.println(i);
+
+        }
+        System.out.println("i am out");
+    }
+    
+}
