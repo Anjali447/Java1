fdb2ab99664e71b641aeddd9310da45c00513eeb
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-12 22:50:44 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,13 @@
+public class Comments {
+    public static void main(String args[]){
+        //this is a single line comment
+        System.err.println("hello world");
+
+    /*
+     * this example uses a multi line comment to explain the code
+     */
+        System.err.println("hey");
+
+    }
+    
+}
