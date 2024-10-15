69719f8511a9f3330eaa359864c35945e59f2083
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-15 11:50:22 +0530

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,24 @@
+public class DataType {
+    public static void main(String[] args) {
+        byte b=127;
+        System.out.println(b);
+
+        char ch='#';
+        System.out.println(ch);
+
+        short n=245;
+        System.out.println(n);
+
+        boolean var=true;
+        System.out.println(var);
+
+        int num=25;
+        System.out.println(num);
+
+        float price= 20.5f;
+        System.out.println(price);
+
+        //long
+        //double
+    }
+}
