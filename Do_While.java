eed79be2729f1fbb7a110bbd30f94dccba56d523
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-16 15:25:58 +0530

    Add files via upload

---
+++ Do_While.java
@@ -0,0 +1,10 @@
+public class Do_While {
+    public static void main(String args[]){
+        int counter = 1;
+
+        do{
+            System.out.println("HELLO WORLD");
+            counter++;
+        } while(counter <= 10);
+    }
+}
