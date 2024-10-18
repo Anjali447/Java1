43653b45e59dae7d85ded3bf720b2e14a83d7913
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-19 00:16:42 +0530

    Add files via upload

---
+++ Explicit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Explicit {
+    public static void main(String[] args) {
+        /*float a = 34.26f;
+        int b = (int)a;
+        System.out.println(b);*/
+        Scanner sc = new Scanner(System.in);
+         long a = sc.nextLong();
+            int b = (int) a;
+            System.out.println(b);
+    }
+    
+}
