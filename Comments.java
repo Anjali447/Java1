2165bfb29c53c21b8516176851d876d6f004b2ca
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-22 23:37:54 +0530

    Update Comments.java

--- Comments.java
+++ Comments.java
@@ -1,8 +1,8 @@
 public class Comments {
     public static void main(String args[]){
         //this is a single line comment
+       
         System.err.println("hello world");
-
     /*
      * this example uses a multi line comment to explain the code
      */
