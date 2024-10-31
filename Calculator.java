e73f3caed1f2f48d2019e7cacb28f11add1210b0
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-10-31 21:56:50 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -13,7 +13,7 @@ public class Calculator {
     char operator = sc.next().charAt(0);
 
     switch (operator) {
-        case '+': System.out.println(a+b);
+          case '+': System.out.println(a+b);
           break;
 
         case '-': System.out.println(a-b);
