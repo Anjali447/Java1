546c94912ecdf6f0d323736ea3dbc82494a5f0e0
Anjali447 <anjali.shrivastava.190@gmail.com>
2024-11-01 15:49:25 +0530

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,10 @@
+// print 10 times hello world using for loop
+
+public class For_loop {
+    public static void main(String args[]) {
+        //int i;
+        for( int i = 0; i <= 10; i++){
+        System.out.println("Hello World");
+       }
+    }
+}
