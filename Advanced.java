5a8fe862b83d04e8fdc0d0b1f3f5a40e216bc02c
Anjali Shrivastava <anjali.shrivastava.190@gmail.com>
2025-11-15 23:16:16 +0530

    Update Advanced.java

--- Advanced.java
+++ Advanced.java
@@ -10,13 +10,3 @@ public class Advanced {
 Names of variables are called identifiers in Java. Identifier rule says, identifiers can start with
 any alphabet or underscore (“_”) or dollar (“$”).
 According to the rule the given variable name is a valid identifier. */
-
-
-
-
-
-
-
-
-
-
